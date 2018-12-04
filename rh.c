061bcc7573b3005006a446da1bc8b37eb600cbff
zdoroven <45197696+zdoroven@users.noreply.github.com>
2018-12-04 12:40:13 +0300

    Add files via upload

---
+++ rh.c
@@ -0,0 +1,41 @@
+/**
+ * rh.c -- программа удаления первой строки
+ *
+ * Copyright (c) 2018, Zdorovennov Maksim <zdoroven@cs.karelia.ru>
+ *
+ * This code is licensed under a MIT-style license.
+ */ 
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include "common.h"
+#include "text/text.h"
+
+static void remove_first_line(int index, char *contents, int cursor, void *data);
+
+static int flag = 1;
+
+void rh (text txt){
+
+    flag = 1;
+
+    process_forward(txt, remove_first_line, txt);
+
+}
+
+static void remove_first_line(int index, char *contents, int cursor, void *data)
+{
+    assert(contents != NULL);
+
+    UNUSED (index);
+
+    UNUSED (cursor);
+    
+    if (flag == 1){
+
+	delete_line(data, 1);
+    }
+
+    flag = 0;
+}
